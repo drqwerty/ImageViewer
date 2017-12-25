3ef3d70bbc481fcf516a15d312c578552acf1419
Dani <Dani@MSI-GL62M-7RD>
2017-12-25 19:09:48 +0000

    Final version Images with automatic scaling!

--- src/view/SwingImageDisplay.java
+++ src/view/SwingImageDisplay.java
@@ -30,7 +30,45 @@ public class SwingImageDisplay extends JPanel implements ImageDisplay {
 
             @Override
             public void paint(Graphics g) {
-                g.drawImage(bitmap(), 0, 0, this.getWidth(), this.getHeight(), null);
+                int imgWidth = bitmap().getWidth(null);
+                int imgHeight = bitmap().getHeight(null);
+
+                double imgAspect = (double) imgHeight / imgWidth;
+
+                int canvasWidth = this.getWidth();
+                int canvasHeight = this.getHeight();
+
+                double canvasAspect = (double) canvasHeight / canvasWidth;
+
+                int x1 = 0; // top left X position
+                int y1 = 0; // top left Y position
+                int x2 = 0; // bottom right X position
+                int y2 = 0; // bottom right Y position
+
+                if (imgWidth < canvasWidth && imgHeight < canvasHeight) {
+                    // the image is smaller than the canvas
+                    x1 = (canvasWidth - imgWidth) / 2;
+                    y1 = (canvasHeight - imgHeight) / 2;
+                    x2 = imgWidth + x1;
+                    y2 = imgHeight + y1;
+
+                } else {
+                    if (canvasAspect > imgAspect) {
+                        y1 = canvasHeight;
+                        // keep image aspect ratio
+                        canvasHeight = (int) (canvasWidth * imgAspect);
+                        y1 = (y1 - canvasHeight) / 2;
+                    } else {
+                        x1 = canvasWidth;
+                        // keep image aspect ratio
+                        canvasWidth = (int) (canvasHeight / imgAspect);
+                        x1 = (x1 - canvasWidth) / 2;
+                    }
+                    x2 = canvasWidth + x1;
+                    y2 = canvasHeight + y1;
+                }
+                g.clearRect(0, 0, this.getWidth(), this.getHeight());
+                g.drawImage(bitmap(), x1, y1, x2, y2, 0, 0, imgWidth, imgHeight, null);
             }
 
             private java.awt.Image bitmap() {
